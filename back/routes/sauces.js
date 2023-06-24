74be4c2570b5dd247c563b2d5876140288b7c4ec
Bazabas Barbara <barbara-bazabas@live.fr>
2023-06-24 13:34:20 +0100

    add auth middleware + fixes

--- back/routes/sauces.js
+++ back/routes/sauces.js
@@ -1,12 +1,13 @@
 const express = require( 'express' );
 const router = express.Router();
+const auth = require( '../middleware/auth' );
 const sauceCtrl = require( '../controllers/sauces' );
 
-router.get( '/', sauceCtrl.getAllSauces );
-router.get( '/:id', sauceCtrl.getOneSauce );
-router.post( '/', sauceCtrl.createSauce );
-router.put( '/:id', sauceCtrl.modifySauce );
-router.delete( '/:id', sauceCtrl.deleteSauce );
+router.get( '/', auth, sauceCtrl.getAllSauces );
+router.get( '/:id', auth, sauceCtrl.getOneSauce );
+router.post( '/', auth, sauceCtrl.createSauce );
+router.put( '/:id', auth, sauceCtrl.modifySauce );
+router.delete( '/:id', auth, sauceCtrl.deleteSauce );
 
 
 module.exports = router
